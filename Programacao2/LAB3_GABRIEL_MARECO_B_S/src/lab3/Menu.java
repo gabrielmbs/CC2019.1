cb23496eacaf35ac68826a1b0a4c58249929d486
Gabriel Mareco <gabriel.souto@ccc.ufcg.edu.br>
2019-04-04 02:59:41 -0300

    Agora funciona

--- Programacao2/LAB3_GABRIEL_MARECO_B_S/src/lab3/Menu.java
+++ Programacao2/LAB3_GABRIEL_MARECO_B_S/src/lab3/Menu.java
@@ -1,15 +1,16 @@
 package lab3;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Menu {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		String entrada = "";
-		Contato[] contatos = new Contato[100];
-		int[] posicoes = new int[100];
-		int contador = 0;
+		Contato[] contatos = new Contato[101];
+		List<Integer> posicoes = new ArrayList<Integer>();
 
 		do {
 			System.out.println("(C)adastrar Contato");
@@ -35,27 +36,28 @@ public class Menu {
 				String sobreNome = teclado.nextLine();
 				System.out.print("Telefone: ");
 				String telefone = teclado.nextLine();
-				System.out.println("CADASTRO REALIZADO!");
+				System.out.println("CADASTRO REALIZADO!\n");
 				
 				contatos[posicao] = new Contato(nome, sobreNome, telefone);
-				posicoes[contador] = posicao;
-				Arrays.sort(posicoes);
+				posicoes.add(posicao);
+				Collections.sort(posicoes);
 
 			} else if (entrada.equals("L")) {
-				
-				for(int i = 0; i < contador; i++) {
-					System.out.format("%d - %s\n", posicoes[i], contatos[posicoes[i]].exibeNome());
+				System.out.println("");
+				for(int i = 0; i < posicoes.size(); i++) {
+					System.out.println(posicoes.get(i) + " - " + contatos[posicoes.get(i)].exibeNome());
 				}
-
+				System.out.println("");
 			} else if (entrada.equals("E")) {
 				
 				System.out.print("Contato> ");
 				int posicao = Integer.parseInt(teclado.nextLine());
 				String imprimir = contatos[posicao].toString();
-				System.out.println(imprimir);
+				System.out.println("\n" + imprimir + "\n");
 
 			} else if (entrada.equals("S")) {
 				break;
+				
 			} else {
 				System.out.println("OPÇÃO INVÁLIDA!\n");
 			}
