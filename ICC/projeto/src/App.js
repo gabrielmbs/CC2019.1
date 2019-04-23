c9b67f7d157ad9ad13ac1b22f69955d7fa18114b
Gabriel Mareco <gabriel.souto@ccc.ufcg.edu.br>
2019-04-23 00:46:35 -0300

    Adicionando projeto icc funcionando, curso de html e css, e alguns testes do projeto icc

--- ICC/projeto/src/App.js
+++ ICC/projeto/src/App.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 
-var data = new Date();
-var dataAtual = data.getDate() + '/' + (data.getMonth()+1) + '/' + data.getFullYear();
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nome: "",
+      presente: "",
+      entendimento: "",
+      comentario: ""
+    };
+    this.onChange = (evento) => {
+      const state = Object.assign({}, this.state);
+      const campo = evento.target.name;
+      state[campo] = evento.target.value;
+      this.setState(state);
+    };
+    this.onSubmit = (evento) => {
+      evento.preventDefault();
+      //validação
+      console.log(this.state);
+    };
+  }
   render() {
     return (
       <div className="App">
-       <h1>{dataAtual}</h1>
-       <div>
-         <h2>Avalie a aula</h2>
-        <button name="button1" value="1" onclick="alert(this.value)">1</button> 
-        <button name="button2">2</button>
-        <button name="button3">3</button>
-        <button name="button4">4</button>
-        <button name="button5">5</button>
+        <head>
+          <title>Avaliação da aula</title>
+        </head>
 
-       </div>
+        <body>
+          <div id="area">
+            <form id="formulario" autoComplete="off">
+              <fieldset>
+                <legend>Avaliação da aula</legend>
+                <label>Nome:</label><input name="nome" className="campo_nome" value={this.state.nome} onChange={this.onChange} type="text" /><br />
+                <label>Matricula:</label><input name="matricula" value={this.state.matricula} onChange={this.onChange} className="campo_matricula" type="texto" /><br />
+                <label>Data da aula:</label><input name="data" type="date" className="campo_data" type="date" /><br />
+                <label>Você estava presente? </label>
+                Sim<input name="presente" type="radio" value="sim" onChange={this.onChange} checked={this.state.presente === "sim"} className="campo_presente_sim"/>
+                Não<input name="presente" type="radio" value="nao" onChange={this.onChange} checked={this.state.presente === "nao"} className="campo_presente_nao" />
+                <br />
+                <label>Entendimento da aula:</label>
+                1<input name="entendimento" type="radio" value="1" onChange={this.onChange} checked={this.state.entendimento === "1"} className="opcao"/>
+                2<input name="entendimento" type="radio" value="2" onChange={this.onChange} checked={this.state.entendimento === "2"} className="opcao"/>
+                3<input name="entendimento" type="radio" value="3" onChange={this.onChange} checked={this.state.entendimento === "3"} className="opcao"/>
+                4<input name="entendimento" type="radio" value="4" onChange={this.onChange} checked={this.state.entendimento === "4"} className="opcao"/>
+                5<input name="entendimento" type="radio" value="5" onChange={this.onChange} checked={this.state.entendimento === "5"} className="opcao"/>
+                <br />
+                1 - Não entendi nada <br />
+                5 - Entendi tudo
+                <br />
+                <label>Deixe um comentário, alguma questão que ficou em dúvida ou algum conteúdo que não entendeu:</label><br /><textarea name="comentario" value={this.state.comentario} onChange={this.onChange} className="campo_comentario" cols="35" rows="8"></textarea><br />
+                <button onClick={this.onSubmit} className="btn_submit" >Enviar</button>
+              </fieldset>
+            </form>
+          </div>
+        </body>
       </div>
     );
   }
---
+++ ICC/projetoTeste/build/precache-manifest.6fba3caf510a7b1e713c79a97355b1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b2f105f4d751668395ae",
+    "url": "/static/js/main.e35eea40.chunk.js"
+  },
+  {
+    "revision": "5aef0e42fd9401a12e67",
+    "url": "/static/js/2.ff73aee1.chunk.js"
+  },
+  {
+    "revision": "b2f105f4d751668395ae",
+    "url": "/static/css/main.8526b7f6.chunk.css"
+  },
+  {
+    "revision": "08037d513ff8e145d8fc0fd0308bdf16",
+    "url": "/index.html"
+  }
+];
