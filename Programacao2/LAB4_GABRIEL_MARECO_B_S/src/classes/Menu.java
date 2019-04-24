eba0a537342cfe9cb4adeb37df233d6b730846d8
Gabriel Mareco <gabriel.souto@ccc.ufcg.edu.br>
2019-04-24 00:38:08 -0300

    Adicionando coisas no Lab4 e arquivo novo de html

--- Programacao2/LAB4_GABRIEL_MARECO_B_S/src/classes/Menu.java
+++ Programacao2/LAB4_GABRIEL_MARECO_B_S/src/classes/Menu.java
@@ -1,10 +1,13 @@
 package classes;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
         String opcao = "";
+        String matricula, nome, curso;
+        HashMap<String, String> mapaMatriculaAlunos = new HashMap<>();
         Scanner teclado = new Scanner(System.in);
 
         do{
@@ -20,6 +23,38 @@ public class Menu {
 
             opcao = teclado.nextLine();
 
+            if(opcao.equals("C")){
+
+                System.out.print("\nMatrícula: ");
+                matricula = teclado.nextLine();
+                System.out.print("Nome: ");
+                nome = teclado.nextLine();
+                System.out.print("Curso: ");
+                curso = teclado.nextLine();
+
+                if(mapaMatriculaAlunos.containsKey(matricula)){
+                    System.out.println("MATRÍCULA JÁ CADASTRADA!");
+                }else{
+                    mapaMatriculaAlunos.put(matricula, (nome + " - " + curso) );
+                    System.out.println("CADASTRO REALIZADO!");
+                }
+
+
+            }else if(opcao.equals("E")){
+                System.out.print("\nMatrícula: ");
+                matricula = teclado.nextLine();
+
+                if (mapaMatriculaAlunos.containsKey(matricula)){
+                    String aluno = matricula + " - " + mapaMatriculaAlunos.get(matricula);
+                    System.out.println("\nAluno: " + aluno);
+                }else{
+                    System.out.println("\nAluno não cadastrado.");
+                }
+
+            }
+
+
         }while(!opcao.equals("O"));
+        teclado.close();
     }
 }
