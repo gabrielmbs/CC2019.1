f8a853ea183c7817dc11fd21c89b351b66f468f6
Gabriel Mareco <gabriel.souto@ccc.ufcg.edu.br>
2019-05-11 01:50:55 -0300

    iniciando testes

---
+++ Programacao2/LAB5_PARTE1_GABRIEL_MARECO_BATISTA_DE_SOUTO/testes/classes_test/ClienteTest.java
@@ -0,0 +1,37 @@
+package classes_test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setLocalizacao() {
+    }
+
+    @Test
+    void toString1() {
+    }
+
+    @Test
+    void equals1() {
+    }
+
+    @Test
+    void hashCode1() {
+    }
+}
