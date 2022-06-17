fd47fb6d55154e41a297f3806c13ad6eb480796e
Gabriel Souto <gabriel.souto@ccc.ufcg.edu.br>
2022-06-17 16:33:40 -0300

    fix lab3

--- Programacao2/LAB3_GABRIEL_MARECO_B_S/src/lab3/Agenda.java
+++ Programacao2/LAB3_GABRIEL_MARECO_B_S/src/lab3/Agenda.java
@@ -3,124 +3,21 @@ package lab3;
 import java.util.Arrays;
 
 /**
-<<<<<<< HEAD
  * Agenda de contatos, classe onde interage com a classe Contato. Ela será
  * responsavel por armazenar o array de contatos e manipular o array de
  * contatos.
-=======
- * Agenda de contatos, classe onde interage com a classe Contato.
- * Ela será responsavel por armazenar o array de contatos
- * e manipular o array de contatos.
->>>>>>> ded18813c34152481774c17c0e09a6ce87041dd6
  *
  * @author Gabriel Mareco Batista de Souto
  */
 public class Agenda {
-<<<<<<< HEAD
-	/**
-	 * Array de contatos do tipo contato, onde iremos salvar e manipular.
-	 */
-	private Contato[] contatos;
-
-	/**
-	 * Constroi uma agenda telefonica, com 100 posicoes. Todos os contatos iniciam
-	 * como null.
-	 *
-	 */
-	public Agenda() {
-		this.contatos = new Contato[100];
-	}
-
-	/**
-	 * Aqui ele salva, ou seja, cadastra um contato no array de contatos.
-	 *
-	 * @param posicao   posicao da agenda desejada
-	 * @param nome      nome do contato a ser salvo
-	 * @param sobreNome sobrenome do contato a ser salvo
-	 * @param telefone  telefone do contato a ser salvo
-	 *
-	 * @return true se cadastrou com sucesso e retorna false se falhou na hora de
-	 *         salvar.
-	 */
-	public boolean cadastrarContato(int posicao, String nome, String sobreNome, String telefone) {
-		if (posicao >= 1 && posicao <= 100) {
-			this.contatos[posicao - 1] = new Contato(nome, sobreNome, telefone);
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Ele procura um contato na agenda e imprime se exitir esse contato. Busca pela
-	 * posicao que o usuario ira entrar.
-	 *
-	 * @param posicao posicao a qual o usuaio deseja exibir.
-	 *
-	 * @return uma string, caso exista, retorna todas as informacoes do contato, no
-	 *         formato 'nome sobrenome - telefone' caso não exista, retorma 'POSICAO
-	 *         INVALIDA!'.
-	 */
-	public String pesquisarContato(int posicao) {
-		if ((posicao < 1 || posicao > 100) || (this.contatos[posicao - 1] == null)) {
-			return "POSIÇÃO INVÁLIDA!\n";
-		} else {
-			return "\n" + this.contatos[posicao - 1].toString() + "\n";
-		}
-	}
-
-	/**
-	 * Lista todos os contatos inseridos na agenda.
-	 *
-	 * @return uma string, no formato, 'posicao - nome sobrenome'
-	 */
-	public String listarContatos() {
-		String listaDeContatos = "\n";
-		for (int i = 0; i < this.contatos.length; i++) {
-			if (this.contatos[i] != null) {
-				listaDeContatos += (i + 1) + " - " + this.contatos[i].exibeNome() + "\n";
-			}
-		}
-		return listaDeContatos;
-
-	}
-
-	/**
-	 * Aqui compara se duas agendas são iguais, elas são iguais, se tiverem os
-	 * mesmos contatos, nas mesmas posicoes.
-	 *
-	 * @param o que vai ser comparado.
-	 * @return true se forem iguais ou false se forem diferentes
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Agenda agenda = (Agenda) o;
-		return Arrays.equals(contatos, agenda.contatos);
-	}
-
-	/**
-	 * Gera o hashcode da agenda, usuando o array de contatos.
-	 *
-	 * @return um inteiro, representando o array.
-	 */
-	@Override
-	public int hashCode() {
-		return Arrays.hashCode(contatos);
-	}
-=======
     /**
      * Array de contatos do tipo contato, onde iremos salvar e manipular.
      */
     private Contato[] contatos;
 
     /**
-     * Constroi uma agenda telefonica, com 100 posicoes.
-     * Todos os contatos iniciam como null.
-     *
+     * Constroi uma agenda telefonica, com 100 posicoes. Todos os contatos iniciam
+     * como null.
      */
     public Agenda() {
         this.contatos = new Contato[100];
@@ -129,35 +26,35 @@ public class Agenda {
     /**
      * Aqui ele salva, ou seja, cadastra um contato no array de contatos.
      *
-     * @param posicao posicao da agenda desejada
-     * @param nome nome do contato a ser salvo
+     * @param posicao   posicao da agenda desejada
+     * @param nome      nome do contato a ser salvo
      * @param sobreNome sobrenome do contato a ser salvo
-     * @param telefone telefone do contato a ser salvo
-     *
-     * @return true se cadastrou com sucesso e retorna false se falhou na hora de salvar.
+     * @param telefone  telefone do contato a ser salvo
+     * @return true se cadastrou com sucesso e retorna false se falhou na hora de
+     * salvar.
      */
     public boolean cadastrarContato(int posicao, String nome, String sobreNome, String telefone) {
-        if(posicao >= 1 && posicao <= 100) {
+        if (posicao >= 1 && posicao <= 100) {
             this.contatos[posicao - 1] = new Contato(nome, sobreNome, telefone);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     /**
-     * Ele procura um contato na agenda e imprime se exitir esse contato.
-     * Busca pela posicao que o usuario ira entrar.
+     * Ele procura um contato na agenda e imprime se exitir esse contato. Busca pela
+     * posicao que o usuario ira entrar.
      *
      * @param posicao posicao a qual o usuaio deseja exibir.
-     *
-     * @return uma string, caso exista, retorna todas as informacoes do contato, no formato 'nome sobrenome - telefone'
-     *         caso não exista, retorma 'POSICAO INVALIDA!'.
+     * @return uma string, caso exista, retorna todas as informacoes do contato, no
+     * formato 'nome sobrenome - telefone' caso não exista, retorma 'POSICAO
+     * INVALIDA!'.
      */
     public String pesquisarContato(int posicao) {
-        if( (posicao < 1 || posicao > 100) || (this.contatos[posicao - 1] == null)){
+        if ((posicao < 1 || posicao > 100) || (this.contatos[posicao - 1] == null)) {
             return "POSIÇÃO INVÁLIDA!\n";
-        }else {
+        } else {
             return "\n" + this.contatos[posicao - 1].toString() + "\n";
         }
     }
@@ -171,7 +68,7 @@ public class Agenda {
         String listaDeContatos = "\n";
         for (int i = 0; i < this.contatos.length; i++) {
             if (this.contatos[i] != null) {
-                listaDeContatos += (i+1) + " - " + this.contatos[i].exibeNome() + "\n";
+                listaDeContatos += (i + 1) + " - " + this.contatos[i].exibeNome() + "\n";
             }
         }
         return listaDeContatos;
@@ -179,16 +76,18 @@ public class Agenda {
     }
 
     /**
-     * Aqui compara se duas agendas são iguais, elas são iguais, se
-     * tiverem os mesmos contatos, nas mesmas posicoes.
+     * Aqui compara se duas agendas são iguais, elas são iguais, se tiverem os
+     * mesmos contatos, nas mesmas posicoes.
      *
      * @param o que vai ser comparado.
      * @return true se forem iguais ou false se forem diferentes
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Agenda agenda = (Agenda) o;
         return Arrays.equals(contatos, agenda.contatos);
     }
@@ -202,5 +101,4 @@ public class Agenda {
     public int hashCode() {
         return Arrays.hashCode(contatos);
     }
->>>>>>> ded18813c34152481774c17c0e09a6ce87041dd6
 }
--- Programacao2/LAB3_GABRIEL_MARECO_B_S/src/lab3/Contato.java
+++ Programacao2/LAB3_GABRIEL_MARECO_B_S/src/lab3/Contato.java
@@ -3,44 +3,12 @@ package lab3;
 import java.util.Objects;
 
 /**
-<<<<<<< HEAD
  * Representa o contato, onde vai guardar todas informções do contato E ira
  * pegar todas as informacoes de um contato, quando agenda precisar.
-=======
- * Representa o contato, onde vai guardar todas informções do contato
- * E ira pegar todas as informacoes de um contato, quando agenda precisar.
->>>>>>> ded18813c34152481774c17c0e09a6ce87041dd6
  *
  * @author Gabriel Mareco Batista de Souto
  */
 public class Contato {
-<<<<<<< HEAD
-	/**
-	 * Representa o nome do contato
-	 */
-	private String nome;
-	/**
-	 * Representa o sobrenome do contato.
-	 */
-	private String sobreNome;
-	/**
-	 * Representa o telefone do contato.
-	 */
-	private String telefone;
-
-	/**
-	 * Constroi um contato apartir do nome, sobrenome e telefone de um contato.
-	 * Retona NullPointerException caso algum parametro seja passado nulo e retorna
-	 * IllegalArgumentException caso o parametro passado seja vazio
-	 *
-	 * @param nome      nome do contato
-	 * @param sobreNome sobrenome do contato
-	 * @param telefone  telefone do contato
-	 */
-	public Contato(String nome, String sobreNome, String telefone) {
-		if (nome == null) {
-			throw new NullPointerException("Nome nulo");
-=======
     /**
      * Representa o nome do contato
      */
@@ -56,114 +24,37 @@ public class Contato {
 
     /**
      * Constroi um contato apartir do nome, sobrenome e telefone de um contato.
+     * Retona NullPointerException caso algum parametro seja passado nulo e retorna
+     * IllegalArgumentException caso o parametro passado seja vazio
      *
-     * @param nome nome do contato
+     * @param nome      nome do contato
      * @param sobreNome sobrenome do contato
-     * @param telefone telefone do contato
+     * @param telefone  telefone do contato
      */
     public Contato(String nome, String sobreNome, String telefone) {
         if (nome == null) {
             throw new NullPointerException("Nome nulo");
->>>>>>> ded18813c34152481774c17c0e09a6ce87041dd6
 
-		} else if (nome.trim().equals("")) {
-			throw new IllegalArgumentException("Nome vazio");
-		}
-		if (sobreNome == null) {
-			throw new NullPointerException("Sobrenome nulo");
+        } else if (nome.trim().equals("")) {
+            throw new IllegalArgumentException("Nome vazio");
+        }
+        if (sobreNome == null) {
+            throw new NullPointerException("Sobrenome nulo");
 
-		} else if (sobreNome.trim().equals("")) {
-			throw new IllegalArgumentException("Sobrenome vazio");
-		}
-		if (telefone == null) {
-			throw new NullPointerException("Telefone nulo");
+        } else if (sobreNome.trim().equals("")) {
+            throw new IllegalArgumentException("Sobrenome vazio");
+        }
+        if (telefone == null) {
+            throw new NullPointerException("Telefone nulo");
 
-		} else if (telefone.trim().equals("")) {
-			throw new IllegalArgumentException("Telefone vazio");
-		}
-		this.nome = nome;
-		this.sobreNome = sobreNome;
-		this.telefone = telefone;
-	}
+        } else if (telefone.trim().equals("")) {
+            throw new IllegalArgumentException("Telefone vazio");
+        }
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+        this.telefone = telefone;
+    }
 
-<<<<<<< HEAD
-	/**
-	 * Ele pega o nome do contato, foi utilizado para testar o construtor.
-	 *
-	 * @return uma string, que representa o nome do contato.
-	 */
-	public String getNome() {
-		return this.nome;
-	}
-
-	/**
-	 * Ele pega o sobrenome do contato, foi utilizado para testar o construtor.
-	 *
-	 * @return uma string, que representa o sobrenome do contato.
-	 */
-	public String getSobreNome() {
-		return this.sobreNome;
-	}
-
-	/**
-	 * Ele pega o telefone do contato, foi utilizado para testar o construtor.
-	 *
-	 * @return uma string, que representa o telefone do contato.
-	 */
-	public String getTelefone() {
-		return this.telefone;
-	}
-
-	/**
-	 * Aqui ele exibe o nome de um contato, utilizado quando agenda pede para listar
-	 * os contato.
-	 *
-	 * @return uma string, no formato 'nome sobrenome'
-	 */
-	public String exibeNome() {
-		return this.nome + " " + this.sobreNome;
-	}
-
-	/**
-	 * Ele retorna uma representacao textual do contato, ou seja, mostra todas as
-	 * informacoes de um contato. Utilizada quando agenda pede para exibir um
-	 * contato.
-	 *
-	 * @return uma string, no formato 'nome sobrenome - telefone'
-	 */
-	@Override
-	public String toString() {
-		return this.nome + " " + this.sobreNome + " " + "-" + " " + this.telefone;
-	}
-
-	/**
-	 * Compara se todos contatos são iguais, para eles serem iguais precisam ter os
-	 * mesmos nomes e sobrenomes.
-	 *
-	 * @param o objeto que vai ser comparado.
-	 * @return retorna true, se forem igais, caso contrario, retorna false.
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Contato contato = (Contato) o;
-		return Objects.equals(nome, contato.nome) && Objects.equals(sobreNome, contato.sobreNome);
-	}
-
-	/**
-	 * Gera um hashCode de um contato, ele utiliza o nome e sobrenome do contato
-	 * para isso.
-	 *
-	 * @return um inteiro, que representa o contato.
-	 */
-	@Override
-	public int hashCode() {
-		return Objects.hash(nome, sobreNome);
-	}
-=======
     /**
      * Ele pega o nome do contato, foi utilizado para testar o construtor.
      *
@@ -192,7 +83,8 @@ public class Contato {
     }
 
     /**
-     * Aqui ele exibe o nome de um contato, utilizado quando agenda pede para listar os contato.
+     * Aqui ele exibe o nome de um contato, utilizado quando agenda pede para listar
+     * os contato.
      *
      * @return uma string, no formato 'nome sobrenome'
      */
@@ -201,8 +93,9 @@ public class Contato {
     }
 
     /**
-     * Ele retorna uma representacao textual do contato, ou seja, mostra todas as informacoes
-     * de um contato. Utilizada quando  agenda pede para exibir um contato.
+     * Ele retorna uma representacao textual do contato, ou seja, mostra todas as
+     * informacoes de um contato. Utilizada quando agenda pede para exibir um
+     * contato.
      *
      * @return uma string, no formato 'nome sobrenome - telefone'
      */
@@ -212,23 +105,25 @@ public class Contato {
     }
 
     /**
-     * Compara se todos contatos são iguais, para eles serem iguais precisam ter
-     * os mesmos nomes e sobrenomes.
+     * Compara se todos contatos são iguais, para eles serem iguais precisam ter os
+     * mesmos nomes e sobrenomes.
      *
      * @param o objeto que vai ser comparado.
      * @return retorna true, se forem igais, caso contrario, retorna false.
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Contato contato = (Contato) o;
-        return Objects.equals(nome, contato.nome) &&
-                Objects.equals(sobreNome, contato.sobreNome);
+        return Objects.equals(nome, contato.nome) && Objects.equals(sobreNome, contato.sobreNome);
     }
 
     /**
-     * Gera um hashCode de um contato, ele utiliza o nome e sobrenome do contato para isso.
+     * Gera um hashCode de um contato, ele utiliza o nome e sobrenome do contato
+     * para isso.
      *
      * @return um inteiro, que representa o contato.
      */
@@ -236,5 +131,4 @@ public class Contato {
     public int hashCode() {
         return Objects.hash(nome, sobreNome);
     }
->>>>>>> ded18813c34152481774c17c0e09a6ce87041dd6
 }
